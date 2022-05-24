1849b30b75780eb0c0adf5b7c23ad4208f6b2405
Pralop <105304041+Pralop@users.noreply.github.com>
2022-05-24 20:37:41 +0700

    update shell

--- Auto_build.sh
+++ Auto_build.sh
@@ -1,53 +1,67 @@
 #!/bin/sh
 LOG=/home/ec2-user/autobuild.log
+rm -f $LOG
 ##ECR Login##
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 628007638077.dkr.ecr.ap-southeast-1.amazonaws.com
 ###VAR##
+TAG_NO=$(echo $1)
 COUNT_IMAGE=$(docker images|wc -l)
 ###Faction###
 CLEAR()
 {
-docker rmi $(docker images -a -q) --force
+CHECK_IM=$(docker images -a |grep -i wordpress|awk '{print $3}'|wc -l)
+if [ $CHECK_IM -eq 1 ]
+then
+        docker rmi $(docker images -a -q) --force|tee -a $LOG
+else
+        echo "Can't clear"
+fi
 }
+
 CLEAR_DOCKER()
 {
-docker stop $(docker ps |grep wordpress|awk '{print $5}')
+docker stop $(docker ps |grep amazon|awk '{print $12}')
 }
 BUILD()
 {
-if[ $COUNT_IMAGE -eq 0 ]
-then 
-    docker build -t Wordpress:$1 .
-else
-    echo "Please clear docker image"
+        docker build -t $TAG_NO .
 }
 
 TAG()
 {
-docker tag Wordpress:$1 628007638077.dkr.ecr.ap-southeast-1.amazonaws.com/pralop-repo:$1
+docker tag $TAG_NO 628007638077.dkr.ecr.ap-southeast-1.amazonaws.com/pralop-repo:$TAG_NO|tee -a $LOG
 }
 
 PUSH()
 {
-docker push 628007638077.dkr.ecr.ap-southeast-1.amazonaws.com/pralop-repo:$1
+docker push 628007638077.dkr.ecr.ap-southeast-1.amazonaws.com/pralop-repo:$TAG_NO|tee -a $LOG
 }
 
 PULL()
 {
-docker pull 628007638077.dkr.ecr.ap-southeast-1.amazonaws.com/pralop-repo:$1
+docker pull 628007638077.dkr.ecr.ap-southeast-1.amazonaws.com/pralop-repo:$TAG_NO|tee -a $LOG
 }
 
 DOCKER_RUN()
 {
-docker run -d -p 80:80 628007638077.dkr.ecr.ap-southeast-1.amazonaws.com/pralop-repo:$1
+docker run -d -p 80:80 628007638077.dkr.ecr.ap-southeast-1.amazonaws.com/pralop-repo:$TAG_NO|tee -a $LOG
 }
 
 ###Main###
-if[ $COUNT_IMAGE != 0 ]
+if [ $COUNT_IMAGE -gt 2 ]
 then
+  CLEAR_DOCKER
+  sleep 10
   CLEAR
+  echo "Prepare Build Image"
+  sleep 10
+  BUILD
+  TAG
+  PUSH
+  DOCKER_RUN
 else
   BUILD
   TAG
   PUSH
   DOCKER_RUN
+fi
